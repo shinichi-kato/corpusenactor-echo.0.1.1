18df669583e00e0bd30e5e47b5dec594f6d7edd8
Shinichi KATO <skato21r@iris.eonet.ne.jp>
2018-02-20 01:01:38 +0900

    UIまでコーディング

--- main.py
+++ main.py
@@ -4,13 +4,14 @@
 ローカルでのテスト
 virtualenv env
 source ./env/bin/activate
-dev_appserver app.yaml
+dev_appserver.py app.yaml
 
 """
 
 from __future__ import unicode_literals
 from __future__ import print_function
 
+import os
 import logging
 import codecs
 from flask import Flask,render_template, request
@@ -40,11 +41,12 @@ def index():
         ce = CorpusEnactor("chatbot/chatbot.yaml")
         reply = ce.reply(text)
 
-        lines.append(text)
-        lines.append(reply)
+        lines.append({'speaker':'user','text':text})
+        lines.append({'speaker':'bot','text':reply})
+        lines = lines[-MAIN_LOG_PRESEVE_LEN:]
 
         try:
-            with codecs.open(MAIN_LOG,'w','utf-8')
+            with codecs.open(MAIN_LOG,'w','utf-8'):
                 fcntl.flock(f,fcntl.LOCK_EX)
                 f.write(yaml.dump(lines,default_flow_style=False))
                 fcntl.flock(f,fcntl.LOCK_UN)
@@ -54,7 +56,7 @@ def index():
             return render_template('retry.html')
 
 
-    return render_template("index.html",reply=reply,text=text)
+    return render_template("index.html",lines=lines[-MAIN_LOG_DISPLAY_LEN:])
 
 
 
